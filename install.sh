0a593435e037c295b7c9ef2aa8bade0eeedb1840
Philip Corliss <pcorliss@50projects.com>
2014-01-16 18:58:13 -0600

    Fixing symlink creation issue

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ echo 'source ~/.phils_bash_settings' >> ~/.bash_profile
 mkdir -p ~/bin
 
 # symlink files in bin directory
-find `pwd`/bin -type f -exec ln -sfn {} ~/bin/ \; 
+find `pwd`/bin -not -type d -exec ln -sfn {} ~/bin/ \;
